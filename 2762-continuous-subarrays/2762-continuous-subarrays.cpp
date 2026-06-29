eb2b655d45f03da0dbe5cc6d4dd22eec227237ec
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-06-29 23:44:55 +0530

    Time: 85 ms (87.75%), Space: 121.7 MB (41.8%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+
+        long long ans = 0;
+        int n = nums.size();
+        int ei = 0,si = 0;
+
+        deque<int>min;
+        deque<int>max;
+        
+        while(ei < n)
+        {
+            while(!min.empty() && nums[min.back()] > nums[ei])
+            {
+                min.pop_back();
+            }
+
+            min.push_back(ei);
+            while(!max.empty() && nums[max.back()] < nums[ei])
+            {
+                max.pop_back();
+            }
+            max.push_back(ei);
+
+            while(nums[max.front()] - nums[min.front()] > 2)
+            {
+                //we need to remove the old indices while shirinking the window as that will be not part of our window
+                if(min.front() == si)
+                {
+                    min.pop_front();
+                }
+
+                if(max.front() == si)
+                {
+                    max.pop_front();
+                }
+                //shrink the window until it is valid
+                si++;
+            }
+            //in this range all paris are guaranteed that diff will be <= 2 by performing diff btw max and min element in the window
+            ans += (ei - si + 1);
+            ei++;
+        }
+
+        return ans;
+    }
+};
