abc57b73d948e3d2c5571dbf27104e57185f004a
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-06-30 11:53:54 +0530

    Time: 21 ms (94.48%), Space: 107.6 MB (26.44%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int>mp;
+        for(int &i : tasks)
+        {
+            mp[i]++;
+        }
+
+        int out = 0;
+
+        for(auto &ele : mp)
+        {
+            if(ele.second <=1)
+            {
+                return -1;
+            }
+            if(ele.second % 3 == 0)
+            {
+                out += ele.second / 3;
+            }
+            else
+            {
+                out += (ele.second / 3) + 1;
+            }
+        }
+
+        return out;
+    }
+};
