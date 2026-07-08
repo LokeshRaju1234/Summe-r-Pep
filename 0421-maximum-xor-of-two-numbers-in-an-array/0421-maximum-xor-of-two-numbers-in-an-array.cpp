2354639c1dbbe9f7c1813839c4babac8983c9ffa
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-07-08 23:59:19 +0530

    Time: 1232 ms (7.81%), Space: 368.5 MB (8.72%) - LeetHub

--- 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
+++ 0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,57 +1,74 @@
-class TrieNode{
+class TrieNode
+{
     public:
     vector<TrieNode*>child;
+    
     TrieNode()
     {
+        //stores two values 0 and 1
         child = vector<TrieNode*>(2,nullptr);
     }
-};
 
+
+};
 class Solution {
 public:
-TrieNode* root;
-
+       TrieNode* root;
+      
     void insert(int n)
-    {
-        TrieNode* tempRoot = root;
-        bitset<32>b(n);
-        for(int i=31;i>=0;i--)
         {
-            if(!tempRoot->child[b[i]])tempRoot->child[b[i]] = new TrieNode();
-        tempRoot = tempRoot->child[b[i]];
+            TrieNode* tempnode = root;
+        bitset<32>b(n);
+        for(int i = 31;i >= 0;i--)
+        {
+            
+            if(tempnode->child[b[i]] == nullptr)
+            {
+                tempnode->child[b[i]] = new TrieNode();
+            }
+
+            tempnode = tempnode->child[b[i]];
         }
-    }
+
+        }
+
 
     int findMaximumXOR(vector<int>& nums) {
-        int ans=0;
+
+        int ans = 0;
         root = new TrieNode();
-        for(auto val : nums)
+
+        for(int ele : nums)
         {
-            insert(val);
+            insert(ele);
         }
 
-        for(auto val : nums)
+        for(int ele : nums)
         {
-            ans=max(ans,findMax(val));
+            ans = max(ans,findMax(ele));
         }
-    return ans;
+        return ans;
     }
 
     int findMax(int n)
     {
-        int curAns=0;
         bitset<32>b(n);
+        int currans = 0;
         TrieNode* tempRoot = root;
-        for(int i=31;i>=0;i--)
+        for(int i = 31;i >= 0;i--)
         {
-            int opp = 1 - b[i];
-            if(tempRoot->child[opp])
+            int tar = 1 - b[i];
+            if(tempRoot->child[tar] != nullptr)
             {
-                curAns = curAns + (1<<i);
-            tempRoot = tempRoot->child[opp];
-            }    
-            else tempRoot = tempRoot->child[b[i]];
+                currans += (1 << i);
+                tempRoot = tempRoot->child[tar];
+            }
+            else
+            {
+                tempRoot = tempRoot->child[b[i]];
+            }
         }
-    return curAns;
+
+        return currans;
     }
 };
