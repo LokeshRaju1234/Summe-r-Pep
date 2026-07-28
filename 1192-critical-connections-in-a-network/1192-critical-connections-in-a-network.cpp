2c9333a3930f20ee97c83f1c3f94854677a8bdda
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-07-28 22:53:41 +0530

    Time: 186 ms (57.67%), Space: 182.1 MB (50.43%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-vector<vector<int>>ans;
-    void dfs(auto& adj,auto& time,int parent,int curr,int &gt)
+    vector<vector<int>> ans;
+    void dfs(auto &adj,auto &time,int parent,int curr,int &t)
     {
-        time[curr] = gt++;
-        int cnt = 0;
+        time[curr] = t++;
         int temp = time[curr];
-        for(auto neigh : adj[curr])
-        {
-            if(neigh == parent && cnt == 0) 
-            {cnt++;continue;}
 
+        for(auto &neigh : adj[curr])
+        {
+            //because it came from the parent only
+            if(parent == neigh) continue;
             if(time[neigh] == -1)
             {
-                dfs(adj,time,curr,neigh,gt);
+                dfs(adj,time,curr,neigh,t);
 
                 time[curr] = min(time[curr],time[neigh]);
 
-                if(time[neigh] > temp) ans.push_back({curr,neigh});
+                if(time[neigh] > temp)
+                {
+                    ans.push_back({curr,neigh});
+                }
             }
-
-           else  time[curr] = min(time[curr], time[neigh]);
+            //it has a back edge and it reaches to the anchester even when you remove this edge
+            else time[curr] = min(time[curr],time[neigh]);
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& c) {
         ans = {};
-        vector<vector<int>> adj(n);
+       vector<vector<int>> adj(n);
 
-        for(int i = 0;i < c.size();i++)
-        {
-            int u = c[i][0];
-            int v = c[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
+       for(auto &edge : c)
+       {
+        int u = edge[0];
+        int v = edge[1];
 
-        int gt = 0;
-        vector<int> time(n,-1);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+       }
 
-        for(int i = 0;i < n;i++)
-        {
-            if(time[i] == -1) dfs(adj,time,-1,i,gt);
-        }
+       vector<int> time(n,-1);
+       int t = 0;
+       for(int i = 0;i < n;i++)
+       {
+        if(time[i] == -1) dfs(adj,time,-1,i,t);
+       }
 
-        return ans;
-
+       return ans;
     }
 };
