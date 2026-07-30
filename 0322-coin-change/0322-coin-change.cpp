b570318c57814d9c13d987d9c31d604736dc849f
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-07-30 15:00:09 +0530

    Time: 45 ms (38.63%), Space: 53 MB (12.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,39 +1,68 @@
 class Solution {
 public:
+    // vector<vector<bool>>dp;
+    // int solve(int i,int t,vector<int>& coins)
+    // {
+    //     if(t == 0)
+    //     {
+    //         return 0;
+    //     }
+
+    //     if(i >= coins.size())
+    //     {
+    //         return INT_MAX;
+    //     }
+
+    //     if(dp[i][t] != -1)
+    //     {
+    //         return dp[i][t];
+    //     }
+
+    //     int nt = solve(i + 1,t,coins);
+    //     int tk = INT_MAX;
+    //     if(t - coins[i] >= 0)
+    //     {
+    //         int res = solve(i,t - coins[i],coins);
+    //         if(res != INT_MAX)
+    //         {
+    //             tk = 1 + res;
+    //         }
+    //     }
+
+    //     return dp[i][t] = min(nt,tk);
+    // }
+
     vector<vector<int>>dp;
-    int solve(int i,int t,vector<int>& coins)
-    {
-        if(t == 0)
+    int coinChange(vector<int>& coins, int amount) {
+        int INF = 1e9 + 7;
+        int n = coins.size();
+        dp.assign(n + 1,vector<int>(amount + 1,INF));
+
+        for(int i = 0;i <= n;i++)
         {
-            return 0;
+            dp[i][0] = 0;
         }
 
-        if(i >= coins.size())
+        for(int i = 1; i <= n;i++)
         {
-            return INT_MAX;
-        }
-
-        if(dp[i][t] != -1)
-        {
-            return dp[i][t];
-        }
-
-        int nt = solve(i + 1,t,coins);
-        int tk = INT_MAX;
-        if(t - coins[i] >= 0)
-        {
-            int res = solve(i,t - coins[i],coins);
-            if(res != INT_MAX)
+            for(int j = 1;j <= amount;j++)
             {
-                tk = 1 + res;
+                if(j - coins[i - 1] >= 0)
+                {
+                    dp[i][j] = min(1 + dp[i][j - coins[i - 1]],dp[i - 1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
 
-        return dp[i][t] = min(nt,tk);
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        dp.assign(coins.size(),vector<int>(amount + 1,-1));
-        int ans = solve(0,amount,coins);
-        return (ans == INT_MAX) ? -1 : ans;
+        if(dp[n][amount] != INF)
+        {
+            return dp[n][amount];
+        }
+
+        return -1;
     }
 };
