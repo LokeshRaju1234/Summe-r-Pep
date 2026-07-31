e81418011069369d77232f9c8c418e585e5ed9cd
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-07-31 10:51:10 +0530

    Time: 4 ms (99.33%), Space: 14.3 MB (79.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -6,30 +6,14 @@ public:
         if(sum %2 == 1) return false;
 
         int t = sum / 2;
-        int n = nums.size();
-        
-        vector<vector<bool>>dp(n + 1,vector<bool>(t + 1,false));
+        bitset<10001>b;
+        b[0] = 1;
 
-        for(int i = 0;i <= n;i++)
+        for(int x : nums)
         {
-            dp[i][0] = true;
+            b |= (b << x);
         }
 
-        for(int i = 1;i <= n;i++)
-        {
-            for(int j = 1;j <= t;j++)
-            {
-                if(j - nums[i - 1] >= 0)
-                {
-                    dp[i][j]  = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                }
-                else
-                {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-
-        return dp[n][t];
+        return b[t];
     }
 };
