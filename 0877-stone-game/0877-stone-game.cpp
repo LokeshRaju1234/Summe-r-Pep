8d969d86f2e61926e1618b33dbd116449e920248
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-08-04 12:38:00 +0530

    Time: 7 ms (37.69%), Space: 19.6 MB (26.43%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public: 
-    vector<vector<int>> dp;
-    int solve(int i,int j,vector<int>& piles)
-    {
-        //if there is only one element remains
-        if(i == j)
-        {
-            return piles[i];
-        }
-
-        if(dp[i][j] != -1)
-        {
-            return dp[i][j];
-        }
-        int left = piles[i] - solve(i + 1,j,piles);
-        int right = piles[j] - solve(i,j - 1,piles);
-
-        return dp[i][j] = max(left,right);
-    }
+    
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        dp.assign(n + 1,vector<int>(n + 1,-1));
-        return solve(0,n - 1,piles) > 0;
+        vector<vector<int>> dp;
+        dp.assign(n,vector<int>(n,1e9 + 7));
+
+        for(int i = 0;i < n;i++)   
+        {
+            //when there is only one index we consider that element
+            dp[i][i] = piles[i];
+        }
+
+        //dp[i][j] = max score difference i can get while playing on piles[i ... j]
+        //fill the larger intervals after filling the base cases
+        for(int i = n - 1;i >= 0;i--)
+        {
+            for(int j =  i + 1;j < n;j++)
+            {
+                //every player has two choices if he takes i and p2 -> i+1 ..j
+                //if p1 chooses j then p2 will play on i ... j - 1
+                int left = piles[i] - dp[i + 1][j];
+                int right = piles[j] - dp[i][j - 1];
+
+                //because the player is playing optimally
+                dp[i][j] = max(left,right);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
     }
 };
