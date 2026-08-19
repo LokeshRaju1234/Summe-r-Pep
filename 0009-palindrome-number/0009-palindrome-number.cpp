7ef3bbe88f6dfe90be30785b67d4a71df8b5e26e
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-08-19 22:31:37 +0530

    Time: 0 ms (100%), Space: 10.8 MB (16.43%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
+    bool isPalindrome(int x) 
+    {
         string pali = to_string(x);
         int n = pali.size();
         int i = 0,j = pali.size() - 1;
