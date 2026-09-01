ffa79f8bfd326e2605fbcc9bbaa0e8e4942f4f6b
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-09-01 12:18:48 +0530

    Time: 4 ms (57.5%), Space: 124.4 MB (96.02%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> result = {-1, -1};
-
-        // Initialize minimum distance to the maximum possible value
-        int minDistance = INT_MAX;
-
-        // Pointers to track the previous node, current node, and indices
-        ListNode* previousNode = head;
-        ListNode* currentNode = head->next;
-        int currentIndex = 1;
-        int previousCriticalIndex = 0;
-        int firstCriticalIndex = 0;
-
-        while (currentNode->next != nullptr) {
-            // Check if the current node is a local maxima or minima
-            if ((currentNode->val < previousNode->val &&
-                 currentNode->val < currentNode->next->val) ||
-                (currentNode->val > previousNode->val &&
-                 currentNode->val > currentNode->next->val)) {
-                // If this is the first critical point found
-                if (previousCriticalIndex == 0) {
-                    previousCriticalIndex = currentIndex;
-                    firstCriticalIndex = currentIndex;
-                } else {
-                    // Calculate the minimum distance between critical points
-                    minDistance =
-                        min(minDistance, currentIndex - previousCriticalIndex);
-                    previousCriticalIndex = currentIndex;
+        //the starting index where we found the critical node we will get max dist by using this by sub the last node found with this first found node
+        int currIndex = 1;
+        int firstcriticalIndex = 0;
+        int lastcriticalIndex = 0;
+        int mindist = INT_MAX;
+        int maxdist = INT_MIN;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        bool found = false;
+        while(curr != NULL){
+            ListNode* nextNode = curr->next;
+            //checking if it is a critical point or not and this is the first time i am finding the critical node
+            if((prev != NULL) && (nextNode != NULL) &&
+           ((curr->val < prev->val) && (curr->val < nextNode->val) || 
+            (curr->val > prev->val) && (curr->val > nextNode->val))){
+                if(found == false)
+                {
+                    //then this is the first critical node we found
+                    firstcriticalIndex = currIndex;
+                    lastcriticalIndex = currIndex;
+                    found = true;
+                }
+                else
+                {
+                    //if it is not the firstCritical node then we can keep on calculating
+                    //the mindist by doing the currCriticalnode  - lastfoundCriticalNode
+                    mindist = min(mindist,currIndex - lastcriticalIndex);
+                    lastcriticalIndex = currIndex;
                 }
             }
 
-            // Move to the next node and update indices
-            currentIndex++;
-            previousNode = currentNode;
-            currentNode = currentNode->next;
+            currIndex++;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        if(mindist != INT_MAX){
+            //by doing the last critical section - the first critical node we found we 
+            //will get the maxxdist
+            maxdist = max(maxdist,lastcriticalIndex - firstcriticalIndex);
         }
 
-        // If at least two critical points were found
-        if (minDistance != INT_MAX) {
-            int maxDistance = previousCriticalIndex - firstCriticalIndex;
-            result = {minDistance, maxDistance};
+        if(mindist != INT_MAX && maxdist != INT_MIN){
+            return {mindist,maxdist};
         }
 
-        return result;
+        return {-1,-1};
     }
 };
