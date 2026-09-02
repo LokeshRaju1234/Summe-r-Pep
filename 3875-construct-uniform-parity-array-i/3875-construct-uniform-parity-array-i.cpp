35c178c82aec8983096c6880eb384f6fd3b38be8
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-09-02 23:41:12 +0530

    Time: 0 ms (100%), Space: 30.4 MB (7.23%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
+        //bcz for every num subtracting with even - odd = oddnumber && odd - even = oddNumber
+        //so for every number it is possible to make this either odd or even array from the array
         return true;
     }
 };
