d4b51fef402d9da8affe4569b87dbfb8d9c724b0
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-09-08 23:58:04 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.38%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -3,6 +3,7 @@ public:
     int countCommas(int n) {
     int count = 0;
 
+    // for every 3 digits there will be 3 commas
     for(int i = 1000;i <= n;i *= 1000){
         count += (n - i + 1);
     }
