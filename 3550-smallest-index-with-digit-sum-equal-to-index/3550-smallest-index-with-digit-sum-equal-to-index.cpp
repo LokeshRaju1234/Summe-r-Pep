6461b6811ed4af6c3f9d8b61da122c783e991ebe
Cholaraju Lokesh Kumar Raju <loki65628@gmail.com>
2026-09-24 22:51:07 +0530

    Time: 0 ms (100%), Space: 30.9 MB (81.66%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int digsum(int num){
+        int sum = 0;
+        while(num > 0){
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum;
+    }
+    int smallestIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        for(int i = 0;i < n;i++){
+            int digSum = digsum(nums[i]);
+            if(digSum == i)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
